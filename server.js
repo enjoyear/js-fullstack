77a2b9f547760bd3683db02d2d4d3a193c1366ca
Chen Guo <almightygc@gmail.com>
2017-01-24 18:33:56 -0800

    alternative 2

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 import config, {nodeEnv, logStars} from './config';
 import http from 'http';
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+    res.write('Hello HTTP!\n');
+    setTimeout(() => {
+        res.write('I can stream!\n');
+    res.end();
+    }, 3000);
+});
 
 server.listen(config.port);
 
-server.on('request', (req, res) => {
-    res.write('Hello HTTP!\n');
-    setTimeout(() => {
-      res.write('I can stream!\n');
-      res.end();
-    }, 3000);
-});
