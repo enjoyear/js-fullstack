d20945630684ab780a85ed36ff382d622052198b
Chen Guo <almightygc@gmail.com>
2017-01-24 18:31:15 -0800

    tmp1

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 import config, {nodeEnv, logStars} from './config';
+import http from 'http';
 
-logStars(config);
-logStars(nodeEnv);
+const server = http.createServer();
+
+server.listen(config.port);
+
+server.on('request', (req, res) => {
+    res.write('Hello HTTP!\n');
+    setTimeout(() => {
+      res.write('I can stream!\n');
+      res.end();
+    }, 3000);
+});
