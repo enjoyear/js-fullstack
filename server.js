e7439f7e9553aac39a55e71e93c5dac01980a69c
Chen Guo <almightygc@gmail.com>
2017-01-24 20:47:48 -0800

    added express server

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-import config, {nodeEnv, logStars} from './config';
-import http from 'http';
+import config from './config';
+import express from 'express';
+import apiRouter from './api';
+const server = express();
 
-const server = http.createServer((req, res) => {
-    res.write('Hello HTTP!\n');
-    setTimeout(() => {
-        res.write('I can stream!\n');
-    res.end();
-    }, 3000);
+server.get('/', (req,res) => {
+  res.send('Hello express');
 });
 
-server.listen(config.port);
+server.use('/api', apiRouter);
+server.use(express.static('public'));
 
+server.listen(config.port, () => {
+  console.info('Express listening on port ' + config.port);
+});
