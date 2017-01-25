d7c074edfa88a4a641c82cb64aa41b236cfa9030
Chen Guo <almightygc@gmail.com>
2017-01-24 21:03:17 -0800

    use ejs template

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 import config from './config';
-import express from 'express';
 import apiRouter from './api';
+
+import express from 'express';
 const server = express();
 
-server.get('/', (req,res) => {
-  res.send('Hello express');
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+  res.render('index', {
+    content: '...'
+  });
 });
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
 
 server.listen(config.port, () => {
-  console.info('Express listening on port ' + config.port);
+  console.info('Express listening on port', config.port);
 });
