ca19e32774ac415646726e5ac4c3714821a4225e
Chen Guo <almightygc@gmail.com>
2017-01-25 18:04:23 -0800

    convert stateless component to stageful component, whose state can be changed in the browser.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import Header from './Header';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header message="Naming Contests" />
-      <div>
-        ...
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { test: 42};
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Header message="Naming Contests" />
+        <div>
+          {this.state.test}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 
 export default App;
