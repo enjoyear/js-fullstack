e648fdef189ae8e50b0a346eb8b54d341ac6c2fb
Chen Guo <almightygc@gmail.com>
2017-01-25 16:01:51 -0800

    install eslint by "apm install linter-eslint"

--- src/index.js
+++ src/index.js
@@ -7,18 +7,19 @@ const App = function(props){
       Hello React from {props.author}{props.defaultX}
     </h2>
   );
-}
+};
 
 App.propTypes = {
   //Error/Warning messages can be found in the browser's console
-  author : React.PropTypes.string.isRequired
-}
+  author : React.PropTypes.string.isRequired,
+  defaultX: React.PropTypes.string
+};
 
 App.defaultProps ={
-  defaultX : "!"
-}
+  defaultX : '!'
+};
 
 ReactDOM.render(
-  <App author={"Chen"}/>,
+  <App author={'Chen'}/>,
   document.getElementById('root')
 );
