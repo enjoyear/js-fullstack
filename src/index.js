e7e7d3ea49a9de1a717e8b559a7662038b62d426
Chen Guo <almightygc@gmail.com>
2017-01-25 16:25:28 -0800

    Refactoring and add components folder.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const App = function(props){
-  return (
-    <h2 className="text-center">
-      Hello React from {props.author}{props.defaultX}
-    </h2>
-  );
-};
-
-App.propTypes = {
-  //Error/Warning messages can be found in the browser's console
-  author : React.PropTypes.string.isRequired,
-  defaultX: React.PropTypes.string
-};
-
-App.defaultProps ={
-  defaultX : '!'
-};
+import App from './components/App';
 
 ReactDOM.render(
-  <App author={'Chen'}/>,
+  <App />,
   document.getElementById('root')
 );
