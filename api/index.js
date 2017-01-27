88f0248ae4856db76c6589d5c4aa7abd8afbf12a
Chen Guo <almightygc@gmail.com>
2017-01-26 18:56:18 -0800

    Add navigation feature to contests - 2.

--- api/index.js
+++ api/index.js
@@ -2,8 +2,17 @@ import express from 'express';
 import data from '../src/testData';
 const router = express.Router();
 
+const reusableContests = data.contests.reduce(
+  (obj, contest) => {
+    obj[contest.id] = contest;
+    return obj;
+  }, {}
+);
+
 router.get('/contests', (req, res) => {
-  res.send({ contests: data.contests });
+  res.send({
+    contests: reusableContests
+  });
 });
 
 export default router;
